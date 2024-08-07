e21ea3d1bf89158f711db6a5f61c8f542e32ac93
unknown <benladen1917@inbox.ru>
2024-08-08 00:18:43 +0300

    feat: morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,9 +37,28 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+const CODE_TABLE = {
+    '00': '',
+    '10': '.',
+    '11': '-',
 }
+
+const decodeMorseLetter = (morseLetter) => {
+    for(let key in MORSE_TABLE) {
+        if (key === morseLetter) {
+            return MORSE_TABLE[key];
+        }
+    }
+};
+
+const decode = expr =>
+    expr.split('**********').map((word) =>
+        word.match(/.{10,10}/g).map((letter) =>
+            decodeMorseLetter(letter.match(/.{2,2}/g)
+                .map((el) => el === '00' ? CODE_TABLE['00'] : el === '10' ? CODE_TABLE['10'] : CODE_TABLE['11'])
+                    .join('')))
+        .join(''))
+    .join(' ');
 
 module.exports = {
     decode
